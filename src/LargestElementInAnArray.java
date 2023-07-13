2471aeb8b65746d9f2d331305585b9af6a498db5
GAURAV SHARMA <gsharma1154@gmail.com>
2023-07-13 13:28:15 +0530

    Largest ELement in array brute force approach

---
+++ src/LargestElementInAnArray.java
@@ -0,0 +1,20 @@
+public class LargestElementInAnArray {
+    static int maxVal(int[] arr){
+        int max=Integer.MIN_VALUE;
+        for (int x:arr
+             ) {
+            if (x>max){
+                max=x;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1={4,6,5,700,4,12,4};
+        int ans=maxVal(arr1);
+        System.out.println(ans);
+
+    }
+
+}
